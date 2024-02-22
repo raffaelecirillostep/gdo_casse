b8bda8a177ad08f380137371214386bfb89acb8b
raffaele_cirillo <raffaele.cirillo@stepsrl.it>
2024-02-22 11:48:11 +0100

    added void project

---
+++ src/main/java/it/step/QuesitoAtsApplication.java
@@ -0,0 +1,13 @@
+package it.step;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuesitoAtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuesitoAtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/step/QuesitoAtsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.step;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuesitoAtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
